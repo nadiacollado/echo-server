19ba6d0ee518cb633e9ed807f8a58d407d5e9c1e
Nadia Collado <collado.nadia@gmail.com>
2021-12-23 18:41:16 -0500

    Refactor EchoServer

--- src/main/java/echoserver/EchoServer.java
+++ src/main/java/echoserver/EchoServer.java
@@ -1,38 +1,96 @@
 package echoserver;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
-import java.net.ServerSocket;
+import java.io.*;
+import java.net.*;
 
 public class EchoServer {
-    public static void main(String[] args) throws IOException {
-        int port = 8080;
+    private static int port = 8080;
+    private static BufferedReader input;
+    private static PrintWriter output;
+    private static Socket clientSocket;
+    private static ServerSocket serverSocket;
 
+    public static void main(String[] args) {
         if (args.length >= 1) {
             port = Integer.parseInt(args[0]);
-        } else if (args.length == 0){
+        } else {
             System.out.println("Port not provided. Using port 8080.");
         }
 
-        ServerSocket serverSocket = null;
         try {
-            serverSocket = new ServerSocket(port);
-        } catch (IOException e) {
-            System.out.println("Issue trying to listen on port" + port);
-            System.out.println(e.getMessage());
-        }
-        System.out.println("Listening for connection...");
-        Socket clientSocket = serverSocket.accept();
-        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            serverSocket = startServerSocket(port);
+            System.out.println("Listening for connection on port " + port);
 
+            while (true) {
+                Socket clientSocket = startClientSocket(serverSocket);
+                startSocketIO(clientSocket);
+            }
+        } catch (IOException e) {
+            System.out.println("Issue trying to listen on port " + port);
+        }
+    }
+
+    public static void startSocketIO(Socket clientSocket) throws IOException {
+        manageIOStream(createSocketWriter(clientSocket), createSocketReader(clientSocket));
+    }
+
+    public static ServerSocket startServerSocket(int port) throws IOException {
+        return new ServerSocket(port);
+    }
+
+    public static Socket startClientSocket(ServerSocket socket) throws IOException {
+        try {
+            clientSocket = socket.accept();
+            System.out.println("Connection successful");
+        } catch (IOException e) {
+            System.out.println("Connection unsuccessful");
+        }
+        return clientSocket;
+    }
+
+    public static PrintWriter createSocketWriter(Socket clientSocket) throws IOException {
+        output = new PrintWriter(clientSocket.getOutputStream(), true);
+        return output;
+    }
+
+    public static BufferedReader createSocketReader(Socket clientSocket) throws IOException {
+        input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        return input;
+    }
+
+    public static void manageIOStream(PrintWriter out, BufferedReader input) throws IOException {
         String inputLine;
-        while ((inputLine = in.readLine()) != null) {
-            System.out.println("Server has received the following input: " + inputLine);
-            out.println(inputLine);
+        while ((inputLine = readFromInputStream(input)) != null) {
+            if (quit(inputLine)) {
+                close();
+                break;
+            }
+            System.out.println("Client input: " + inputLine);
+            writeToOutputStream(out, inputLine);
+        }
+    }
+
+    public static String readFromInputStream(BufferedReader input) throws IOException {
+        return input.readLine();
+    }
+
+    public static void writeToOutputStream(PrintWriter out, String data) {
+        out.println("Server output: " + data);
+    }
+
+    public static boolean quit(String keyword) {
+        keyword = keyword.toLowerCase();
+        return (keyword.equals("q") || keyword.equals("quit"));
+    }
+
+    public static void close() {
+        try {
+            input.close();
+            output.close();
+            clientSocket.close();
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
